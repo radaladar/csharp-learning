0a667ebfd8c930462b42b7923af5ea8dafe6ac19
radaladar <bbiczok@gmail.com>
2017-03-25 14:32:41 +0100

    hello world, i hope i'm doing this right

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            helloWorld();
+        }
+
+        static void helloWorld()
+        {
+            Console.WriteLine("What is your name?");
+            Console.Write("> ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
